6b04413c6a9f20207a64c7ae0801b6f7a563fccc
Rasmus Enevoldsen <rasmus@enev.dk>
2026-02-28 09:42:41 +0100

    No FSH

--- audiorelay.nix
+++ audiorelay.nix
@@ -1,57 +1,56 @@
 {
   lib,
-  stdenv,
-  buildFHSEnv,
-  makeWrapper,
+  stdenvNoCC,
+  makeBinaryWrapper,
   fetchurl,
+  xorg,
+  gtk3,
+  gdk-pixbuf,
+  atk,
+  glib,
+  zlib,
+  libGL,
+  fontconfig,
+  freetype,
+  alsa-lib,
+  libpulseaudio,
+
 }:
-let
-  audiorelayRunEnv = buildFHSEnv {
-    name = "audiorelay-env";
-
-    targetPkgs =
-      pkgs:
-      (with pkgs; [
-        xorg.libX11
-        xorg.libXext
-        xorg.libXrender
-        xorg.libXtst
-        xorg.libXi
-        xorg.libXxf86vm
-
-        gtk3
-        gdk-pixbuf
-        atk
-        glib
-        zlib
-        libGL
-        stdenv.cc.cc.lib
-
-        fontconfig
-        freetype
-        alsa-lib
-        libpulseaudio
-      ]);
-
-    runScript = "";
-  };
-in
-stdenv.mkDerivation rec {
+stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "audiorelay";
   version = "0.27.5";
 
   src = fetchurl {
-    url = "https://dl.audiorelay.net/setups/linux/${pname}-${version}.tar.gz";
+    url = "https://dl.audiorelay.net/setups/linux/${finalAttrs.pname}-${finalAttrs.version}.tar.gz";
     sha256 = "sha256-xIVBOaS9Iee/eIGntuIevEz+gjKGeD1Pua1L9O346Mc=";
   };
 
   nativeBuildInputs = [
-    makeWrapper
+    makeBinaryWrapper
   ];
-
-  dontStrip = true;
-  dontPatchELF = true;
   dontAutoPatchelf = true;
+
+  buildInputs = [
+    xorg.libX11
+    xorg.libXext
+    xorg.libXrender
+    xorg.libXtst
+    xorg.libXi
+    xorg.libXxf86vm
+
+    gtk3
+    gdk-pixbuf
+    atk
+    glib
+    zlib
+    libGL
+
+    fontconfig
+    freetype
+    alsa-lib
+    libpulseaudio
+
+  ];
 
   unpackPhase = ''
     mkdir -p audiorelay/
@@ -59,29 +58,44 @@ stdenv.mkDerivation rec {
   '';
 
   installPhase = ''
-    runHook preInstall
+      runHook preInstall
 
-    mkdir -p $out
-    cp -R audiorelay/* $out
+      mkdir -p $out
+      cp -R audiorelay/* $out
 
-    mv $out/lib/app/AudioRelay.cfg $out/lib/app/AudioRelay-unwrapped.cfg
+      mv $out/lib/app/AudioRelay.cfg $out/lib/app/AudioRelay-unwrapped.cfg
+
+      # for FILE in $(ls $out/bin); do
+      #   FILE_PATH="$out/bin/$FILE"
+      #   if [[ -x "$FILE_PATH" ]]; then
+      #     mv "$FILE_PATH" "$FILE_PATH-unwrapped"
+      #     makeBinaryWrapper "$FILE_PATH-unwrapped" "$FILE_PATH" \
+      #         --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath finalAttrs.buildInputs}" 
+      #   fi
+      # done
 
     for FILE in $(ls $out/bin); do
       FILE_PATH="$out/bin/$FILE"
       if [[ -x "$FILE_PATH" ]]; then
         mv "$FILE_PATH" "$FILE_PATH-unwrapped"
-        makeWrapper ${audiorelayRunEnv}/bin/audiorelay-env "$FILE_PATH" \
-          --add-flags "$FILE_PATH-unwrapped"
+        patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+          "$FILE_PATH-unwrapped"
+        makeBinaryWrapper "$FILE_PATH-unwrapped" "$FILE_PATH" \
+            --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath finalAttrs.buildInputs}" \
+            --prefix LD_LIBRARY_PATH : "$out/lib/runtime/lib" \
+            --prefix LD_LIBRARY_PATH : "$out/lib/runtime/lib/server"
       fi
     done
 
-    runHook postInstall
+      runHook postInstall
   '';
 
   meta = {
     description = "Stream audio between your devices.";
     homepage = "https://audiorelay.net/";
     license = lib.licenses.unfree;
+    platforms = [ "x86_64-linux" ];
+    mainProgram = "AudioRelay";
     maintainers = with lib.maintainers; [ ];
   };
-}
+})
